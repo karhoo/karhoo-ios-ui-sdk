ac59a2e8b0795a1b5f1289b983ddb4c76378077b
nurito-burrito <44758040+nurito-burrito@users.noreply.github.com>
2020-09-29 09:04:49 +0100

    Update KarhooUISDK/Common/TableViewDelegateDataSource/TableDataSource.swift
    Co-authored-by: Jeevan Thandi <jeevanthandi@googlemail.com>

--- KarhooUISDK/Common/TableViewDelegateDataSource/TableDataSource.swift
+++ KarhooUISDK/Common/TableViewDelegateDataSource/TableDataSource.swift
@@ -43,7 +43,6 @@ final class TableDataSource<T>: NSObject, UITableViewDataSource {
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
-        
         cell.contentView.isUserInteractionEnabled = false
 
         if let item = tableData.getItem(indexPath: indexPath) {
