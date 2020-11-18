967d033f8d962f8e1c3f362fd671d0c8933ba42b
Nurseda Balcioglu <nurseda.balcioglu@karhoo.com>
2020-11-18 18:08:23 +0000

    allow3ds work done & failing tests fixed

--- KarhooUISDKTests/KarhooSDKService/Quote/MockQuoteService.swift
+++ KarhooUISDKTests/KarhooSDKService/Quote/MockQuoteService.swift
@@ -10,9 +10,13 @@ import Foundation
 import KarhooSDK
 
 final class MockQuoteService: QuoteService {
-
     let quotesPollCall = MockPollCall<Quotes>()
+    let coverageCall = MockCall<QuoteCoverage>()
+    let verifyQuoteCall = MockCall<Quote>()
     var quoteSearchSet: QuoteSearch?
+    var quoteCoverageSet: QuoteCoverageRequest?
+    var verifyQuoteSet: VerifyQuotePayload?
+    
     func quotes(quoteSearch: QuoteSearch) -> PollCall<Quotes> {
         quoteSearchSet = quoteSearch
         return quotesPollCall
@@ -21,5 +25,15 @@ final class MockQuoteService: QuoteService {
     func quotesV2(quoteSearch: QuoteSearch) -> PollCall<Quotes> {
         quoteSearchSet = quoteSearch
         return quotesPollCall
+    }
+    
+    func coverage(coverageRequest: QuoteCoverageRequest) -> Call<QuoteCoverage> {
+        quoteCoverageSet = coverageRequest
+        return coverageCall
+    }
+    
+    func verifyQuote(verifyQuotePayload: VerifyQuotePayload) -> Call<Quote> {
+        verifyQuoteSet = verifyQuotePayload
+        return verifyQuoteCall
     }
 }
