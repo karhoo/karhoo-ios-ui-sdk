2e4959e768f3ca13d2e237bf8810f51d93dd026d
Diana Petrea <diana.petrea@karhoo.com>
2021-11-19 10:57:48 +0200

    set feature flags for testing

--- KarhooUISDK/Screens/CheckoutScreen/Checkout/KarhooCheckoutViewController.swift
+++ KarhooUISDK/Screens/CheckoutScreen/Checkout/KarhooCheckoutViewController.swift
@@ -272,6 +272,46 @@ final class KarhooCheckoutViewController: UIViewController, CheckoutView {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         showCheckoutView(true)
+        setEarnLoyaltyForShowcase()
+    }
+    
+    var canEarn = true
+    var canBurn = true
+    private func setEarnLoyaltyForShowcase() {
+        let earnSwitch = UISwitch()
+        earnSwitch.isOn = true
+        earnSwitch.setOn(true, animated: false)
+        earnSwitch.addTarget(self, action: #selector(onEarnSwitchValueChanged), for: .valueChanged)
+        
+        let alert = UIAlertController.create(title: "Set Loyalty Flags", message: "Choose a value for CAN_EARN", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .cancel, handler: { action in
+            self.setBurnLoyaltyForShowcase()
+        }))
+        alert.view.addSubview(earnSwitch)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    private func setBurnLoyaltyForShowcase() {
+        let burnSwitch = UISwitch()
+        burnSwitch.isOn = true
+        burnSwitch.setOn(true, animated: false)
+        burnSwitch.addTarget(self, action: #selector(onBurnSwitchValueChanged), for: .valueChanged)
+        
+        let alert = UIAlertController.create(title: "Set Loyalty Flags", message: "Choose a value for CAN_BURN", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .cancel, handler: { action in
+            let loyaltyViewModel = LoyaltyViewModel(loyaltyId: "", currency: "GBP", tripAmount: 0, canEarn: self.canEarn, canBurn: self.canBurn)
+            self.loyaltyView.set(viewModel: loyaltyViewModel)
+        }))
+        alert.view.addSubview(burnSwitch)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    @objc private func onEarnSwitchValueChanged(_ swt: UISwitch) {
+        canEarn = swt.isOn
+    }
+    
+    @objc private func onBurnSwitchValueChanged(_ swt: UISwitch) {
+        canBurn = swt.isOn
     }
     
     override func viewDidLoad() {
@@ -345,8 +385,8 @@ final class KarhooCheckoutViewController: UIViewController, CheckoutView {
         termsConditionsView.setBookingTerms(supplier: quote.fleet.name, termsStringURL: quote.fleet.termsConditionsUrl)
         cancellationInfoLabel.text = viewModel.freeCancellationMessage
         farePriceInfoView.setInfoText(for: quote.quoteType)
-        let loyaltyViewModel = LoyaltyViewModel(loyaltyId: "", currency: quote.price.currencyCode, tripAmount: quote.price.highPrice, canEarn: true, canBurn: true)
-        loyaltyView.set(viewModel: loyaltyViewModel)
+        let loyaltyViewModel = LoyaltyViewModel(loyaltyId: "", currency: quote.price.currencyCode, tripAmount: quote.price.highPrice, canEarn: self.canEarn, canBurn: self.canBurn)
+        self.loyaltyView.set(viewModel: loyaltyViewModel)
     }
     
     func set(price: String?) {
