7774ea9b2ca0c3edf792f294579df7542a3164dd
Diana Petrea <diana.petrea@karhoo.com>
2021-12-08 15:38:32 +0200

    fix currency conversion

--- KarhooUISDK/UIComponents/LoyaltyView/KarhooLoyaltyPresenter.swift
+++ KarhooUISDK/UIComponents/LoyaltyView/KarhooLoyaltyPresenter.swift
@@ -83,9 +83,9 @@ final class KarhooLoyaltyPresenter: LoyaltyPresenter {
             return
         }
 
-        // Convert $ amount to cents
-        let amount = viewModel.tripAmount * 100
-        loyaltyService.getLoyaltyEarn(identifier: viewModel.loyaltyId, currency: viewModel.currency, amount: Int(amount), burnPoints: 0).execute { [weak self] result in
+        // Convert $ amount to minor units (cents)
+        let amount = CurrencyCodeConverter.minorUnitAmount(from: viewModel.tripAmount, currencyCode: viewModel.currency)
+        loyaltyService.getLoyaltyEarn(identifier: viewModel.loyaltyId, currency: viewModel.currency, amount: amount, burnPoints: 0).execute { [weak self] result in
             guard let value = result.successValue()
             else {
                 self?.viewModel?.canEarn = false
