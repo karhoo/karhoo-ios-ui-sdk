ae0a3f6ccc707cb517cba85d34433da2326c881b
Diana Petrea <diana.petrea@karhoo.com>
2022-01-05 16:38:36 +0200

    temp commit

---
+++ KarhooUISDK/CommonUI/UIDimens.swift
@@ -0,0 +1,30 @@
+//
+//  UIDimens.swift
+//  KarhooUISDK
+//
+//  Created by Diana Petrea on 05.01.2022.
+//  Copyright © 2022 Flit Technologies Ltd. All rights reserved.
+//
+
+import Foundation
+
+public struct UIDimens {
+    public struct Spacing {
+        public static let extraSmall: CGFloat = 4 //5
+        public static let small: CGFloat = 8
+        public static let medium: CGFloat = 12
+        public static let standard: CGFloat = 16
+        public static let large: CGFloat = 32
+        public static let extraLarge: CGFloat = 64
+    }
+    
+    public struct Size {
+        public static let buttonHeight: CGFloat = 44
+        public static let largeButtonHeight: CGFloat = 64
+        public static let iconSize: CGFloat = 22
+        public static let smallIconSize: CGFloat = 11
+        public static let separatorHeight: CGFloat = 1
+        public static let headerHeight: CGFloat = 90
+        public static let addressTableRowHeight: CGFloat = 50
+    }
+}
