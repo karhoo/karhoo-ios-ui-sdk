b804f5e74fcc0b300327edc574d2d515198a84da
Diana Petrea <diana.petrea@karhoo.com>
2022-01-06 17:55:27 +0200

    compile list of dimens to be used when refactoring the UI

--- KarhooUISDK/CommonUI/UIDimens.swift
+++ KarhooUISDK/CommonUI/UIDimens.swift
@@ -8,38 +8,48 @@
 
 import Foundation
 
-// Note: When updating the UI, aproximate the commented values to the ones before each comment. E.g.: replace any spacing of value 17 to .standard (which is 16)
+// Note: When updating the UI, aproximate the commented values to the ones before each comment. E.g.: replace any spacing of value 17 with .standard (which is 16)
 public struct UIDimens {
     public struct Spacing {
-        public static let xxSmall: CGFloat = 2 //1
+        public static let xxxSmall: CGFloat = 1
+        public static let xxSmall: CGFloat = 2
         public static let xSmall: CGFloat = 4 //3...5
-        public static let small: CGFloat = 8 //6...9
-        public static let medium: CGFloat = 12 //10...14
-        public static let standard: CGFloat = 16 //15...20
-        public static let large: CGFloat = 24 //21...28
+        public static let small: CGFloat = 8 //6...10
+        public static let medium: CGFloat = 12 //11...14
+        public static let standard: CGFloat = 16 //15...21
+        public static let large: CGFloat = 24 //22...28
         public static let xLarge: CGFloat = 32 //29...40
         public static let xxLarge: CGFloat = 44 //41...56
         public static let xxxLarge: CGFloat = 64 // 57+
+        public static let marginToEdgeOfScreen: CGFloat = 20
     }
     
     public struct Size {
         public struct Button {
+            public static let small: CGFloat = 24
             public static let standard: CGFloat = 44
-            public static let large: CGFloat = 64
-            public static let xLarge: CGFloat = 128
+            public static let large: CGFloat = 55 //e.g.: the Book button in the checkout screen
+            public static let xlarge: CGFloat = 64
+            public static let xxLarge: CGFloat = 128
         }
         
         public struct Icon {
             public static let small: CGFloat = 11 //8...14
             public static let medium: CGFloat = 16 //15...17
             public static let standard: CGFloat = 22 //18...26
-            public static let large: CGFloat = 35 //27...43
+            public static let large: CGFloat = 32 //27...39
+            public static let xLarge: CGFloat = 44 //44...53
+            public static let xxLarge: CGFloat = 64 //54+
+            public static let logoWidth: CGFloat = 222
         }
         
         public struct Separator {
             public static let separatorHeight: CGFloat = 1 // 0.5...2
-            public static let borderWidth: CGFloat = 0.5
-            public static let wideBorderWidth: CGFloat = 1.25
+            public static let largeSeparatorHeight: CGFloat = 3
+            public static let noBorder: CGFloat = 0
+            public static let mediumBorderWidth: CGFloat = 0.5
+            public static let standardBorderWidth: CGFloat = 1
+            public static let largeBorderWidth: CGFloat = 1.25
         }
         
         public struct SafeArea {
@@ -66,25 +76,29 @@ public struct UIDimens {
         }
         
         public struct View {
+            public static let tabSelectionHeight: CGFloat = 4
+            public static let loadingHudSize: CGFloat = 22
+            public static let loadingViewHeight: CGFloat = 200 // KarhooQuoteListViewController > loadingView
             public static let headerHeight: CGFloat = 90
             public static let rowHeight: CGFloat = 50 //includes KarhooQuoteListViewController > prebookQuotesTitleLabel height
             public static let largeRowHeight: CGFloat = 60 // includes the address search bar height & KarhooQuoteListViewController > quoteCategoryBarView height
-            public static let loadingViewHeight: CGFloat = 200 // KarhooQuoteListViewController > loadingView
             public static let tagHeight: CGFloat = 20 // KarhooQuoteCategoryBarView > markerView height
-            public static let spinnerSize: CGFloat = 212
+            public static let sideMenuWidth: CGFloat = 280
             
             // TODO: think of better names
+            public static let findTripSpinnerSize: CGFloat = 212
             public static let fleetAvailabilityHeight: CGFloat = 150 //KarhooBookingViewController > bottomNotificationViewBottomConstraint
             public static let quoteListHeight: CGFloat = 230 //QuoteListPanelLayout
             public static let quoteListTopOffset: CGFloat = 140 //QuoteListPanelLayout
             public static let cancelTripProgressViewWidth: CGFloat = 162 //KarhooCancelButtonView
+            public static let driverDetailsViewSize: CGFloat = 180
         }
     }
     
     public struct CornerRadius {
-        public static let small: CGFloat = 4 //5
+        public static let small: CGFloat = 4 //3...5
         public static let medium: CGFloat = 8
-        public static let large: CGFloat = 10 //try 12
+        public static let large: CGFloat = 10
     }
     
     //Animation duration
@@ -95,10 +109,11 @@ public struct UIDimens {
         public static let long: CGFloat = 0.45
         public static let xLong: CGFloat = 0.5
         
-        public static let delay: CGFloat = 0.25
-        public static let mediumDelay: CGFloat = 0.3
-        public static let xLongDelay: CGFloat = 3
-        public static let xxLongDelay: CGFloat = 4
+        public static let xxSmallDelay: CGFloat = 0.1
+        public static let xSmallDelay: CGFloat = 0.25
+        public static let smallDelay: CGFloat = 0.3
+        public static let xxLongDelay: CGFloat = 3
+        public static let xxxLongDelay: CGFloat = 4
     }
     
     public struct Alpha {
@@ -106,5 +121,6 @@ public struct UIDimens {
         public static let enabled: CGFloat = 1
         public static let disabled: CGFloat = 0.4
         public static let shadow: CGFloat = 0.5
+        public static let overlay: CGFloat = 0.6
     }
 }
