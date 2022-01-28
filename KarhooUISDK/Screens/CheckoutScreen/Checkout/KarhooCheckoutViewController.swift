aaa0b12575b5cf5cc8a90650c11b8ee642d65281
mionescu95 <69837374+mionescu95@users.noreply.github.com>
2022-01-28 10:23:37 +0200

    Update KarhooCheckoutViewController.swift

--- KarhooUISDK/Screens/CheckoutScreen/Checkout/KarhooCheckoutViewController.swift
+++ KarhooUISDK/Screens/CheckoutScreen/Checkout/KarhooCheckoutViewController.swift
@@ -81,6 +81,10 @@ final class KarhooCheckoutViewController: UIViewController, CheckoutView {
         footerView.translatesAutoresizingMaskIntoConstraints = false
         footerView.accessibilityIdentifier = "footer_view"
         footerView.backgroundColor = .white
+        footerView.layer.shadowColor = KarhooUI.colors.textLabel.cgColor
+        footerView.layer.shadowOpacity = 1
+        footerView.layer.shadowOffset = CGSize(width:0,height:-1)
+        footerView.layer.shadowRadius = 0.5
         return footerView
     }()
     
@@ -312,8 +316,8 @@ final class KarhooCheckoutViewController: UIViewController, CheckoutView {
         poiDetailsInputText.anchor(leading: baseStackView.leadingAnchor, trailing: baseStackView.trailingAnchor, paddingLeft: standardSpacing, paddingRight: standardSpacing)
         commentsInputText.anchor(leading: baseStackView.leadingAnchor, trailing: baseStackView.trailingAnchor, paddingLeft: standardSpacing, paddingRight: standardSpacing)
         
-        footerView.anchor(leading: view.leadingAnchor, bottom: container.bottomAnchor, trailing: view.trailingAnchor, paddingBottom: standardPadding, paddingRight: smallPadding)
-        footerStack.anchor(top: footerView.topAnchor, leading: footerView.leadingAnchor, bottom: footerView.bottomAnchor, trailing: footerView.trailingAnchor)
+        footerView.anchor(leading: view.leadingAnchor, bottom: container.bottomAnchor, trailing: view.trailingAnchor, paddingBottom: standardPadding)
+        footerStack.anchor(top: footerView.topAnchor, leading: footerView.leadingAnchor, bottom: footerView.bottomAnchor, trailing: footerView.trailingAnchor, paddingTop: standardPadding, paddingBottom: standardPadding)
         termsConditionsView.anchor(leading: baseStackView.leadingAnchor, trailing: baseStackView.trailingAnchor)
     }
     
