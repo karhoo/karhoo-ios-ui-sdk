f8d22bc999ee09884f5da5f5caae951d4cc46208
Bartłomiej Sopala <bartlomiej.sopala@karhoo.com>
2022-02-06 12:07:04 +0100

    add discardableResult

--- KarhooUISDK/Workers/KarhooLegalNoticeLinkOpener.swift
+++ KarhooUISDK/Workers/KarhooLegalNoticeLinkOpener.swift
@@ -18,18 +18,22 @@ final class KarhooLegalNoticeLinkOpener {
         self.viewControllerToPresentFrom = viewControllerToPresentFrom
     }
     
-    func openLink(link: String) {
+    @discardableResult func openLink(link: String) -> Bool{
         guard let linkType = linkParser.getLinkType(link) else {
             assertionFailure("Try to open incorrect link format in KarhooLegalNoticeLinkOpener")
-            return
+            return false
         }
         switch linkType {
         case .url(let url):
             if UIApplication.shared.canOpenURL(url){
                 UIApplication.shared.open(url)
+                return true
+            } else {
+                return false
             }
         case .mail(let addrees):
             _ = KarhooLegalNoticeMailComposer(parent: viewControllerToPresentFrom).sendLegalNoticeMail(addrees: addrees)
+            return true
         }
     }
 }
