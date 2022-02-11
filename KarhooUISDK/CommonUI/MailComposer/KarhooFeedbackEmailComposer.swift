8630e5ad6664d71e4062a8604ee331a98c9a6adb
Bartłomiej Sopala <97114085+BartlomiejSopalaKarhoo@users.noreply.github.com>
2022-02-11 09:07:22 +0100

    Mob 3820 legal notice placeholder (#247)
    * refactor learn more button
    
    * use KarhooExpandViewButton for "Learn more" button
    
    * legal notice view added
    
    * add tap recognizer and mailComposer
    
    * fix paddings
    
    * update translations
    
    * update keys and provide test translations for legal notice
    
    * remove bottom spacing for hidden text
    
    * link parser and mail validator + mai validator tests
    
    * Link Parser tests
    
    * Add Legal notice Link opener
    
    * update translations
    
    * update text
    
    * create KarhooMailMetaInfoComposer
    
    * change variable name
    
    * fixes
    
    * pr fixes 2
    
    * add discardableResult
    
    * pr fixes 3
    
    * change platform name
    
    * remove reference to view controller from legal notice view
    
    * document function
    
    * replace view controller with base view controller in email composers
    
    * update translations from Phrase.com
    
    * fix texts
    
    * move logic to presenter
    
    * remove potential retain cycles
    
    * add initial state for expand button. Init legal notice view in open state
    
    * change legal notice to view controller
    
    * add legal notice presenter
    
    * update legal notice presenter

--- KarhooUISDK/CommonUI/MailComposer/KarhooFeedbackEmailComposer.swift
+++ KarhooUISDK/CommonUI/MailComposer/KarhooFeedbackEmailComposer.swift
@@ -13,17 +13,19 @@ import MessageUI
 public protocol FeedbackEmailComposer: AnyObject {
     func showFeedbackMail() -> Bool
     func reportIssueWith(trip: TripInfo) -> Bool
-    func set(parent: UIViewController)
+    func set(parent: BaseViewController)
     func showNoCoverageEmail() -> Bool
 }
 
 public final class KarhooFeedbackEmailComposer: NSObject, FeedbackEmailComposer {
 
-    private weak var viewController: UIViewController?
+    private weak var viewController: BaseViewController?
 
     private let mailComposer = MailComposer()
+    
+    private var mailMetaInfoComposer: MailMetaInfoComposer = KarhooMailMetaInfoComposer()
 
-    public func set(parent: UIViewController) {
+    public func set(parent: BaseViewController) {
         self.viewController = parent
     }
 
@@ -35,7 +37,7 @@ public final class KarhooFeedbackEmailComposer: NSObject, FeedbackEmailComposer 
         _ = mailComposer.presentMailController(from: viewController!,
                                                subject: UITexts.SupportMailMessage.feedbackEmailSubject,
                                                recipients: [UITexts.SupportMailMessage.feedbackEmailAddress],
-                                               body: mailMetaInfo())
+                                               body: mailMetaInfoComposer.getMailMetaInfo())
         return true
     }
 
@@ -65,20 +67,7 @@ public final class KarhooFeedbackEmailComposer: NSObject, FeedbackEmailComposer 
 
     // MARK: private
 
-    private func mailMetaInfo() -> String {
-        let info = """
-                   \n \n \n
-                   \(UITexts.SupportMailMessage.feedbackMailMessage)
-                   ------------------
-                   Application: \(appName()) \(appVersion())
-                   Device: \(platform())
-                   System: \(systemName()) \(systemVersion())
-                   Locale: \(currentLocale())
-                   User: \(userInfo())
-                   ------------------
-                   """
-        return info
-    }
+    
 
     private func tripReportBody(trip: TripInfo) -> String {
         let info = """
@@ -86,65 +75,8 @@ public final class KarhooFeedbackEmailComposer: NSObject, FeedbackEmailComposer 
                    ------------------
                    Trip: \(trip.displayId)
                    ------------------
-                   \(mailMetaInfo())
+                   \(mailMetaInfoComposer.getMailMetaInfo())
                    """
         return info
-    }
-
-    private func userInfo() -> String {
-        guard let user = Karhoo.getUserService().getCurrentUser() else {
-            return ""
-        }
-
-        return """
-               \(UITexts.User.email): \(user.email)
-               \(UITexts.User.mobilePhone): \(user.mobileNumber)
-               \(UITexts.User.firstName): \(user.firstName)
-               \(UITexts.User.lastName): \(user.lastName)
-               """
-    }
-}
-
-private extension KarhooFeedbackEmailComposer {
-    func appName() -> String {
-        guard let dictionary = Bundle.main.infoDictionary else {
-                    return ""
-        }
-        if let version: String = dictionary["CFBundleDisplayName"] as? String {
-            return version
-        } else {
-            return ""
-        }
-    }
-    
-    func appVersion() -> String {
-        guard let dictionary = Bundle.main.infoDictionary else {
-                    return ""
-        }
-        if let version: String = dictionary["CFBundleShortVersionString"] as? String {
-            return version
-        } else {
-            return ""
-        }
-    }
-
-    func systemName() -> String {
-        return UIDevice.current.systemName
-    }
-
-    func systemVersion() -> String {
-        return UIDevice.current.systemVersion
-    }
-
-func currentLocale() -> String {
-        return NSLocale.current.identifier
-    }
-
-    func platform() -> String {
-        var sysinfo = utsname()
-        uname(&sysinfo) // ignore return value
-        return String(bytes: Data(bytes: &sysinfo.machine,
-                                  count: Int(_SYS_NAMELEN)),
-                      encoding: .ascii)!.trimmingCharacters(in: .controlCharacters)
     }
 }
