e04d44beebfcb6115d3f47d681ecaf1bc748da0b
Aleksander Wędrychowski <aleksander.wedrychowski@karhoo.com>
2022-05-25 17:13:39 +0200

    Feature/MOB-4115 create new target for cicd to test and distribute spm version (#327)
    * ClientSPM project added to repository
    
    * Change ClientSPM deployment target to 14.0

---
+++ ClientSPM/ClientSPM/ClientSPMApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClientSPMApp.swift
+//  ClientSPM
+//
+//  Created by Aleksander Wedrychowski on 20/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct ClientSPMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
