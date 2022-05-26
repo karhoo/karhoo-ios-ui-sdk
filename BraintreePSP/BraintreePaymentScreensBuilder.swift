a5c5ca13174e60248a9b822ab7b73cd485885ac0
Bartłomiej Sopala <bartlomiej.sopala@karhoo.com>
2022-05-26 14:40:35 +0200

    handle cocoapods / spm in different way

--- BraintreePSP/BraintreePaymentScreensBuilder.swift
+++ BraintreePSP/BraintreePaymentScreensBuilder.swift
@@ -32,25 +32,26 @@ final public class BraintreePaymentScreenBuilder: PaymentScreenBuilder {
         guard let flowItem = BTDropInController(authorization: paymentsToken.token,
                                                 request: request,
                                                 handler: {( _, result: BTDropInResult?, error: Error?) in
-            #if SWIFT_PACKAGE
+            /// TODO: remove when Cocoapods version of Braintree will be updated
+            ///
+            /// In Braintree version < 9.0.0 property name is 'isCancelled' and strarting from v 9.0.0 is updated to 'isCanceled'.
+            /// We are using for now version 8.2.0 for Cocoapods so we need to compute this value based on Braintree version
+            var isResultCanceled: Bool? {
+                #if SWIFT_PACKAGE
+                result?.isCanceled
+                #else
+                result?.isCancelled
+                #endif
+            }
+            
             if error != nil {
                 paymentMethodAdded?(.failed(error: error as? KarhooError))
-            } else if result?.isCanceled == true {
+            } else if isResultCanceled == true {
                 paymentMethodAdded?(.cancelled(byUser: true))
             } else {
                 let nonce = Nonce(nonce: result!.paymentMethod!.nonce, cardType: result!.paymentMethod!.type, lastFour: String(result!.paymentDescription.suffix(2)))
                 paymentMethodAdded?(ScreenResult.completed(result: nonce))
             }
-            #else
-            if error != nil {
-                paymentMethodAdded?(.failed(error: error as? KarhooError))
-            } else if result?.isCancelled == true {
-                paymentMethodAdded?(.cancelled(byUser: true))
-            } else {
-                let nonce = Nonce(nonce: result!.paymentMethod!.nonce, cardType: result!.paymentMethod!.type, lastFour: String(result!.paymentDescription.suffix(2)))
-                paymentMethodAdded?(ScreenResult.completed(result: nonce))
-            }
-            #endif
         }) else {
             flowItemCallback?(.failed(error: nil))
             return
