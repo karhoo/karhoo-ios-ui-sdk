7de354851a86e36f5ddea46306bd7ef46072dd71
Aleksander Wędrychowski <aleksander.wedrychowski@karhoo.com>
2022-05-27 10:45:01 +0200

    ClientSPM configuration added

--- ClientSPM/ClientSPM/ClientSPMApp.swift
+++ ClientSPM/ClientSPM/ClientSPMApp.swift
@@ -5,13 +5,55 @@
 //  Created by Aleksander Wedrychowski on 20/05/2022.
 //
 
+import KarhooSDK
+import KarhooUISDK
+import AdyenPSP
 import SwiftUI
 
 @main
 struct ClientSPMApp: App {
+
+    init() {
+        setupKarhooConfig()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+
+    private func setupKarhooConfig() {
+        KarhooUI.set(configuration: KarhooConfig())
+        let guestSettings = GuestSettings(identifier: "",
+                                          referer: "",
+                                          organisationId: "")
+        KarhooConfig.auth = .guest(settings: guestSettings)
+        KarhooConfig.environment = .sandbox
+        KarhooConfig.paymentManager = AdyenPaymentManager()
+    }
+}
+
+final class KarhooConfig: KarhooUISDKConfiguration {
+    static var auth: AuthenticationMethod = .karhooUser
+    static var environment: KarhooEnvironment = .sandbox
+    static var isExplicitTermsAndConditionsApprovalRequired: Bool = false
+    static var paymentManager: PaymentManager!
+
+
+    var isExplicitTermsAndConditionsConsentRequired: Bool {
+        KarhooConfig.isExplicitTermsAndConditionsApprovalRequired
+    }
+
+    func environment() -> KarhooEnvironment {
+        KarhooConfig.environment
+    }
+
+    func authenticationMethod() -> AuthenticationMethod {
+        KarhooConfig.auth
+    }
+    
+    var paymentManager: PaymentManager! {
+        KarhooConfig.paymentManager
     }
 }
