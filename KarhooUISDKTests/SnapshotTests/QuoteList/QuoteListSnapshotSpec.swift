b7b769fb32e1a23f0d00ebdb5077a036361d427a
Aleksander Wedrychowski <aleksander.wedrychowski@karhoo.com>
2022-10-17 19:11:45 +0200

    Add QuoteList snapshot testing files

---
+++ KarhooUISDKTests/SnapshotTests/QuoteList/QuoteListSnapshotSpec.swift
@@ -0,0 +1,9 @@
+//
+//  QuoteListSnapshotSpec.swift
+//  KarhooUISDKTests
+//
+//  Created by Aleksander Wedrychowski on 17/10/2022.
+//  Copyright © 2022 Flit Technologies Ltd. All rights reserved.
+//
+
+import Foundation
