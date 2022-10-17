7b5639552999381c0f93ab9d9761c9562e087109
Aleksander Wedrychowski <aleksander.wedrychowski@karhoo.com>
2022-10-17 19:12:01 +0200

    QuoteList snapshot files

--- KarhooUISDKTests/SnapshotTests/QuoteList/QuoteListSnapshotSpec.swift
+++ KarhooUISDKTests/SnapshotTests/QuoteList/QuoteListSnapshotSpec.swift
@@ -6,4 +6,93 @@
 //  Copyright © 2022 Flit Technologies Ltd. All rights reserved.
 //
 
-import Foundation
+import Quick
+import Nimble
+import SnapshotTesting
+import KarhooSDK
+
+@testable import KarhooUISDK
+
+class QuoteListSnapshotSpec: QuickSpec {
+
+    override func spec() {
+        describe("QuoteList") {
+            var sut: KarhooQuoteListViewController!
+            var presenterMock: MockQuoteListPresenter!
+
+            beforeEach {
+                sut = KarhooQuoteListViewController()
+                presenterMock = MockQuoteListPresenter()
+                sut.setupBinding(presenterMock)
+            }
+
+            context("when the view is opened") {
+
+                it("should have valid design") {
+                    testSnapshot(sut)
+                }
+            }
+        }
+    }
+}
+
+class MockQuoteListPresenter: QuoteListPresenter {
+    var onStateUpdatedCallback: () -> Void = {}
+    lazy var onStateUpdated: ((KarhooUISDK.QuoteListState) -> Void)? = { _ in
+        self.onStateUpdatedCallback()
+    }
+
+    var onFiltersCountUpdatedCallback: () -> Void = {}
+    lazy var onFiltersCountUpdated: ((Int) -> Void)? = { _ in
+        self.onFiltersCountUpdatedCallback()
+    }
+
+    var isSortingAvailableToReturn = false
+    var isSortingAvailable: Bool { isSortingAvailableToReturn }
+
+    func viewDidLoad() {
+    }
+
+    func viewWillAppear() {
+    }
+
+    func viewWillDisappear() {
+    }
+
+    var getNumberOfResultsForQuoteFiltersToReturn = 0
+    func getNumberOfResultsForQuoteFilters(_ filters: [KarhooUISDK.QuoteListFilter]) -> Int {
+        getNumberOfResultsForQuoteFiltersToReturn
+    }
+
+    var selectedQuoteFiltersCalled = false
+    func selectedQuoteFilters(_ filters: [KarhooUISDK.QuoteListFilter]) {
+        selectedQuoteFiltersCalled = true
+    }
+
+    var didSelectQuoteSortOrderCalled = false
+    func didSelectQuoteSortOrder(_ order: KarhooUISDK.QuoteListSortOrder) {
+        didSelectQuoteSortOrderCalled = true
+    }
+
+    var didSelectQuoteCalled = false
+    func didSelectQuote(_ quote: KarhooSDK.Quote) {
+        didSelectQuoteCalled = true
+    }
+
+    var didSelectQuoteDetails = false
+    func didSelectQuoteDetails(_ quote: KarhooSDK.Quote) {
+        didSelectQuoteDetails = true
+    }
+
+    var didSelectShowSortCalled = false
+    func didSelectShowSort() {
+        didSelectShowSortCalled = true
+    }
+
+    var didSelectShowFiltersCalled = false
+    func didSelectShowFilters() {
+        didSelectShowFiltersCalled = true
+    }
+
+
+}
