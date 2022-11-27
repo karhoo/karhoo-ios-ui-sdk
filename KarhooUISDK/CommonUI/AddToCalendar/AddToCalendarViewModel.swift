46522dcc23b08d35229fd881fd2214e71aa3c290
Aleksander Wedrychowski <aleksander.wedrychowski@karhoo.com>
2022-11-27 15:50:28 +0100

    Initial version of add-to-calendar view

---
+++ KarhooUISDK/CommonUI/AddToCalendar/AddToCalendarViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  AddToCalendarViewModel.swift
+//  KarhooUISDK
+//
+//  Created by Aleksander Wedrychowski on 25/11/2022.
+//  Copyright © 2022 Flit Technologies Ltd. All rights reserved.
+//
+
+import Foundation
---
+++ KarhooUISDK/Workers/AddToCalendarWorker.swift
@@ -0,0 +1,9 @@
+//
+//  AddToCalendarWorker.swift
+//  KarhooUISDK
+//
+//  Created by Aleksander Wedrychowski on 27/11/2022.
+//  Copyright © 2022 Flit Technologies Ltd. All rights reserved.
+//
+
+import Foundation
