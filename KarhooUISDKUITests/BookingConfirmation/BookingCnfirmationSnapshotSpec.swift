9498d1c6e9a3a99960c29e45ad409ca68bad6efc
Aleksander Wedrychowski <aleksander.wedrychowski@karhoo.com>
2022-12-02 08:44:25 +0100

    New file for booking confirmation tests

---
+++ KarhooUISDKUITests/BookingConfirmation/BookingCnfirmationSnapshotSpec.swift
@@ -0,0 +1,18 @@
+//
+//  BookingCnfirmationSnapshotSpec.swift
+//  KarhooUISDKUITests
+//
+//  Created by Aleksander Wedrychowski on 02/12/2022.
+//  Copyright © 2022 Flit Technologies Ltd. All rights reserved.
+//
+
+import Quick
+import Nimble
+import SnapshotTesting
+import KarhooSDK
+import KarhooUISDKTestUtils
+@testable import KarhooUISDK
+
+class BookingCnfirmationSnapshotSpec: QuickSpec {
+
+}
