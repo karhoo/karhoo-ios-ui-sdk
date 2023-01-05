c63d2a3162d2836dbf50da032423df64dc5e0d3c
Aleksander Wedrychowski <aleksander.wedrychowski@karhoo.com>
2023-01-05 17:23:13 +0100

    - "Metered" replaced with "metered price" - booking confirmation "Ride details" button accessibility value changed to "Go to ride details"

--- KarhooUISDK/Screens/PrebookConfirmationScreen/KarhooBookingConfirmationView.swift
+++ KarhooUISDK/Screens/PrebookConfirmationScreen/KarhooBookingConfirmationView.swift
@@ -70,6 +70,7 @@ struct KarhooBookingConfirmationView: View {
             KarhooMainButton(title: UITexts.Booking.prebookConfirmedRideDetails) {
                 viewModel.dismiss()
             }
+            .accessibilityValue(Text(UITexts.Booking.prebookConfirmedRideDetailsAccessibility))
         }
         .padding(.vertical, UIConstants.Spacing.standard)
     }
