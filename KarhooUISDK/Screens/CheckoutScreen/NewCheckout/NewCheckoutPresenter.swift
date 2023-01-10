19b6b69d8527f7c71bcd69c6724d35c7a7f76fe8
Aleksander Wedrychowski <aleksander.wedrychowski@karhoo.com>
2023-01-10 10:56:57 +0100

    NEw checkout presenter initial version

--- KarhooUISDK/Screens/CheckoutScreen/NewCheckout/NewCheckoutPresenter.swift
+++ KarhooUISDK/Screens/CheckoutScreen/NewCheckout/NewCheckoutPresenter.swift
@@ -7,10 +7,85 @@
 //
 
 import Foundation
+import KarhooSDK
+import UIKit
+import SwiftUI
 
+enum NewCheckoutState {
+    case loading
+    case asap
+    case scheduled
+}
 final class KarhooNewCheckoutPresenter: NewCheckoutPresenter {
 
+    // MARK: - Properties
+
+    private let callback: ScreenResultCallback<KarhooCheckoutResult>
+    private weak var view: CheckoutView?
+    private let quote: Quote
+    private let journeyDetails: JourneyDetails
+    private let quoteValidityWorker: QuoteValidityWorker
+    internal var passengerDetails: PassengerDetails!
+    private let threeDSecureProvider: ThreeDSecureProvider?
+    private let tripService: TripService
+    private let userService: UserService
+    private let bookingMetadata: [String: Any]?
+    private let paymentNonceProvider: PaymentNonceProvider
+    private let sdkConfiguration: KarhooUISDKConfiguration
+    private let analytics: Analytics
+    private let appStateNotifier: AppStateNotifierProtocol
+    private var trip: TripInfo?
+    private var comments: String?
+    private var bookingRequestInProgress: Bool = false
+    private var flightDetailsScreenIsPresented: Bool = false
+    private let baseFareDialogBuilder: PopupDialogScreenBuilder
+    private var cardRegistrationFlow: CardRegistrationFlow
+
+    // MARK: - Init & Config
+
+    init(
+        quote: Quote,
+        journeyDetails: JourneyDetails,
+        bookingMetadata: [String: Any]?,
+        quoteValidityWorker: QuoteValidityWorker = KarhooQuoteValidityWorker(),
+        threeDSecureProvider: ThreeDSecureProvider? = nil,
+        tripService: TripService = Karhoo.getTripService(),
+        userService: UserService = Karhoo.getUserService(),
+        passengerDetails: PassengerDetails? = PassengerInfo.shared.getDetails(),
+        analytics: Analytics = KarhooUISDKConfigurationProvider.configuration.analytics(),
+        appStateNotifier: AppStateNotifierProtocol = AppStateNotifier(),
+        baseFarePopupDialogBuilder: PopupDialogScreenBuilder = UISDKScreenRouting.default.popUpDialog(),
+        paymentNonceProvider: PaymentNonceProvider = PaymentFactory().nonceProvider(),
+        sdkConfiguration: KarhooUISDKConfiguration =  KarhooUISDKConfigurationProvider.configuration,
+        cardRegistrationFlow: CardRegistrationFlow = PaymentFactory().getCardFlow(),
+        callback: @escaping ScreenResultCallback<KarhooCheckoutResult>
+    ) {
+        self.threeDSecureProvider = threeDSecureProvider ?? sdkConfiguration.paymentManager.threeDSecureProvider
+        self.tripService = tripService
+        self.callback = callback
+        self.userService = userService
+        self.quoteValidityWorker = quoteValidityWorker
+        self.passengerDetails = passengerDetails ?? PassengerInfo.shared.currentUserAsPassenger()
+        self.paymentNonceProvider = paymentNonceProvider
+        self.sdkConfiguration = sdkConfiguration
+        self.appStateNotifier = appStateNotifier
+        self.analytics = analytics
+        self.baseFareDialogBuilder = baseFarePopupDialogBuilder
+        self.quote = quote
+        self.journeyDetails = journeyDetails
+        self.bookingMetadata = bookingMetadata
+        self.cardRegistrationFlow = cardRegistrationFlow
+    }
+
     func viewDidLoad() {
-        
+       quoteValidityWorker.setQuoteValidityDeadline(quote) {
+           // TODO: handle validity expiration
+       }
+    }
+
+    // MARK: - Helpers
+
+    private var isKarhooUser: Bool {
+        Karhoo.configuration.authenticationMethod() == .karhooUser
     }
 }
