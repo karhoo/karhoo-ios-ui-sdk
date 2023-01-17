fa4fe360a2d0e7979fd2dcb51ec288103882a8ca
Aleksander Wędrychowski <aleksander.wedrychowski@karhoo.com>
2023-01-17 14:28:41 +0100

    Feature/mob-4564 new checkout - happy path handling (#478)
    * Legal Notice link hanlding
    
    * Happy path flow
    
    * clean
    
    * Comment

--- KarhooUISDK/Screens/CheckoutScreen/NewCheckout/NewCheckoutCoordinator.swift
+++ KarhooUISDK/Screens/CheckoutScreen/NewCheckout/NewCheckoutCoordinator.swift
@@ -21,6 +21,8 @@ final class KarhooNewCheckoutCoordinator: NewCheckoutCoordinator {
     private(set) var viewController: NewCheckoutViewController!
     private(set) var presenter: KarhooNewCheckoutViewModel?
 
+    private var callback: ((ScreenResult<KarhooCheckoutResult>) -> Void)?
+
     // MARK: - Initializator
 
     init(
@@ -28,19 +30,19 @@ final class KarhooNewCheckoutCoordinator: NewCheckoutCoordinator {
         quote: Quote,
         journeyDetails: JourneyDetails,
         bookingMetadata: [String: Any]?,
-        callback: @escaping ScreenResultCallback<KarhooCheckoutResult>
+        callback: @escaping (ScreenResult<KarhooCheckoutResult>) -> Void
     ) {
         self.navigationController = navigationController
         self.presenter = KarhooNewCheckoutViewModel(
             quote: quote,
             journeyDetails: journeyDetails,
             bookingMetadata: bookingMetadata,
-            router: self,
-            callback: callback
+            router: self
         )
         self.viewController = KarhooNewCheckoutViewController().then {
             $0.setupBinding(presenter!)
         }
+        self.callback = callback
     }
 
 }
@@ -61,5 +63,76 @@ extension KarhooNewCheckoutCoordinator: NewCheckoutRouter {
         viewController.view.backgroundColor = UIColor.clear
         viewController.modalPresentationStyle = .overFullScreen
         baseViewController.present(viewController, animated: true, completion: nil)
+    }
+
+    func routeToPassengerDetails(
+        _ currentDetails: PassengerDetails?,
+        delegate: PassengerDetailsDelegate?
+    ) {
+        let detailsViewController = KarhooComponents.shared.passengerDetails(details: currentDetails, delegate: delegate)
+        baseViewController.showAsOverlay(item: detailsViewController, animated: true)
+    }
+
+    func routeSuccessScene(
+        with tripInfo: TripInfo,
+        journeyDetails: JourneyDetails?,
+        quote: Quote,
+        loyaltyInfo: KarhooBookingConfirmationViewModel.LoyaltyInfo
+    ) {
+        if let journeyDetails, journeyDetails.isScheduled {
+            routeToBookingConfirmation(
+                trip: tripInfo,
+                quote: quote,
+                journeyDetails: journeyDetails,
+                loyaltyInfo: loyaltyInfo
+            )
+        } else {
+            callback?(ScreenResult.completed(result: KarhooCheckoutResult(tripInfo: tripInfo)))
+        }
+    }
+
+    /// Dismiss current flow and take user back to booking map
+    func routeToShowBooking() {
+        navigationController?.popToRootViewController(animated: true, completion: {})
+    }
+
+    func routeToBookingConfirmation(
+        trip: TripInfo?,
+        quote: Quote,
+        journeyDetails: JourneyDetails,
+        loyaltyInfo: KarhooBookingConfirmationViewModel.LoyaltyInfo?
+    ) {
+        guard
+            let viewController = viewController,
+            let navigationController = viewController.navigationController
+        else {
+            assertionFailure()
+            return
+        }
+        let masterViewModel = KarhooBottomSheetViewModel(
+            title: UITexts.Booking.prebookConfirmed
+        ) {
+            viewController.dismiss(animated: true, completion: {
+                self.routeToShowBooking()
+            })
+        }
+
+        let slaveViewModel = KarhooBookingConfirmationViewModel(
+            journeyDetails: journeyDetails,
+            quote: quote,
+            trip: trip,
+            loyaltyInfo: loyaltyInfo ?? .init(shouldShowLoyalty: false, loyaltyPoints: 0, loyaltyMode: .none)
+        ) {
+            viewController.dismiss(animated: true, completion: {
+                self.routeToShowBooking()
+            })
+        }
+
+        let screenBuilder = UISDKScreenRouting.default.bottomSheetScreen()
+        let sheet = screenBuilder.buildBottomSheetScreenBuilderForUIKit(viewModel: masterViewModel) {
+            KarhooBookingConfirmationView(viewModel: slaveViewModel)
+        }
+        //        reportBookingConfirmationScreenOpened(tripId: trip?.tripId, quoteId: quote.id)
+        viewController.present(sheet, animated: true)
     }
 }
