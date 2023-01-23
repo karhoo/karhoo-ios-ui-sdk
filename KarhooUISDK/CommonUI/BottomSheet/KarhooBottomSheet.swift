c2e17f1870562ba409c57d077912c42e95064cca
Bartlomiej Sopala <bartlomiej.sopala@karhoo.com>
2023-01-23 18:04:22 +0100

    redo bottom sheet

--- KarhooUISDK/CommonUI/BottomSheet/KarhooBottomSheet.swift
+++ KarhooUISDK/CommonUI/BottomSheet/KarhooBottomSheet.swift
@@ -22,106 +22,103 @@ struct KarhooBottomSheet<Content: View>: View {
     }
    
     // MARK: - Body
+    
     var body: some View {
-        VStack(spacing: 0.0, content: {
+        ZStack {
             Spacer()
-                .background(Color.clear)
-                .layoutPriority(Double(UILayoutPriority.defaultLow.rawValue))
-            
-            VStack(spacing: 0.0) {
-                HStack {
-                    Text(viewModel.title)
-                        .font(Font(KarhooUI.fonts.title2Bold()))
-                        .foregroundColor(Color(KarhooUI.colors.text))
-                    Spacer()
-                        .background(Color.clear)
-                    Button {
-                        viewModel.dismiss()
-                    } label: {
-                        Image(
-                            uiImage:
-                                UIImage.uisdkImage("kh_uisdk_cross_new")
-                                .coloured(withTint: KarhooUI.colors.text)
-                        )
-                    }
-                    .frame(
-                        width: UIConstants.Dimension.Button.standard,
-                        height: UIConstants.Dimension.Button.standard
+                .background(
+                    LinearGradient(
+                        colors: [
+                            Color(KarhooUI.colors.black).opacity(UIConstants.Alpha.hidden),
+                            Color(KarhooUI.colors.black).opacity(UIConstants.Alpha.enabled)
+                        ],
+                        startPoint: .top,
+                        endPoint: .bottom
                     )
-                    .accessibilityLabel(Text(UITexts.Generic.closeTheScreen))
+                )
+                .edgesIgnoringSafeArea([.all])
+                .opacity(2 - Double(offset.height / 100))
+                .gesture(
+                    DragGesture()
+                        .onChanged({ gesture in
+                            if gesture.translation.height > 0 {
+                                offset = gesture.translation
+                            }
+                        })
+                        .onEnded({ _ in
+                            if offset.height > 100 {
+                                viewModel.dismiss()
+                            } else {
+                                offset = .zero
+                            }
+                        })
+                )
+                .onTapGesture {
+                    viewModel.dismiss()
                 }
-                .padding(
-                    EdgeInsets(
-                        top: UIConstants.Spacing.standard,
-                        leading: UIConstants.Spacing.standard,
-                        bottom: 0,
-                        trailing: UIConstants.Spacing.small
-                    )
-                )
-                
-                // The content is not wrapped inside a ScrollView intentionally
-                // A ScrollView expands to the full height available which is not a desired outcome for all instances where this bottom sheet will be used
-                // Trying to control the height of the ScrollView with GeometryReader gives odd behaviours to the UI, so it is not recommended
-                // If the content view runs the risk of going off screen either because of the amount of content or because a keyboard needs to be opened,
-                // then wrap that view inside a ScrollView before injecting it into this bottom sheet
-                content()
-                    .padding(UIConstants.Spacing.standard)
-
+            
+            VStack(spacing: 0) {
                 Spacer()
-                    .frame(height: getBottomPadding())
-                    .animation(.easeOut(duration: UIConstants.Duration.medium))
+                    .background(Color.clear)
+                VStack(spacing: 0) {
+                    HStack {
+                        Text(viewModel.title)
+                            .font(Font(KarhooUI.fonts.title2Bold()))
+                            .foregroundColor(Color(KarhooUI.colors.text))
+                        Spacer()
+                            .background(Color.clear)
+                        Button {
+                            viewModel.dismiss()
+                        } label: {
+                            Image(
+                                uiImage:
+                                    UIImage.uisdkImage("kh_uisdk_cross_new")
+                                    .coloured(withTint: KarhooUI.colors.text)
+                            )
+                        }
+                        .frame(
+                            width: UIConstants.Dimension.Button.standard,
+                            height: UIConstants.Dimension.Button.standard
+                        )
+                        .accessibilityLabel(Text(UITexts.Generic.closeTheScreen))
+                    }
+                    .padding(
+                        EdgeInsets(
+                            top: UIConstants.Spacing.standard,
+                            leading: UIConstants.Spacing.standard,
+                            bottom: 0,
+                            trailing: UIConstants.Spacing.small
+                        )
+                    )
+                    content()
+                        .padding(UIConstants.Spacing.standard)
+                }
+                // first backround with rounded corners
+                .background(viewModel.backgroundColor.ignoresSafeArea())
+                .cornerRadius(viewModel.cornerRadius, corners: [.topLeft, .topRight])
+                //second background created to fill bottom safe area
+                .padding(.top, -viewModel.cornerRadius)
+                .background(viewModel.backgroundColor.ignoresSafeArea())
+//                .frame(
+//                    maxHeight: UIScreen.main.bounds.height - getTopPadding()
+//                )
+                .offset(x: 0, y: offset.height)
+                .onTapGesture {
+                    // Do nothing. This was added to intercept touches so that they aren't picked up by the main VStack tap gesture recognizer
+                    // .allowsHitTesting(false) does not work for SwiftUI views presented from UIKit views
+                }
             }
-            .background(viewModel.backgroundColor)
-            .clipShape(
-                RoundedRectangle(
-                    cornerRadius: viewModel.cornerRadius,
-                    style: .continuous
-                )
-            )
             .frame(
-                maxWidth: UIScreen.main.bounds.width,
-                maxHeight: UIScreen.main.bounds.height - getTopPadding(),
-                alignment: .bottom
+                maxHeight: UIScreen.main.bounds.height - getTopPadding()
             )
-            .layoutPriority(Double(UILayoutPriority.defaultHigh.rawValue))
-            .offset(x: 0, y: offset.height)
-            .onTapGesture {
-                // Do nothing. This was added to intercept touches so that they aren't picked up by the main VStack tap gesture recognizer
-                // .allowsHitTesting(false) does not work for SwiftUI views presented from UIKit views
-            }
-        })
-        .colorScheme(.light) // Delete this line after dark mode modifications
-        .background(
-            LinearGradient(
-                colors: [
-                    Color(KarhooUI.colors.black).opacity(UIConstants.Alpha.hidden),
-                    Color(KarhooUI.colors.black).opacity(UIConstants.Alpha.enabled)
-                ],
-                startPoint: .top,
-                endPoint: .bottom
-            )
-        )
-        .edgesIgnoringSafeArea([.all])
-        .opacity(2 - Double(offset.height / 100))
-        .gesture(
-            DragGesture()
-                .onChanged({ gesture in
-                    if gesture.translation.height > 0 {
-                        offset = gesture.translation
-                    }
-                })
-                .onEnded({ _ in
-                    if offset.height > 100 {
-                        viewModel.dismiss()
-                    } else {
-                        offset = .zero
-                    }
-                })
-        )
-        .onTapGesture {
-            viewModel.dismiss()
         }
+        .colorScheme(.light) // Delete this line after dark mode modifications
+
     }
+    
+    
+    
+    var body2: some View
     
     // MARK: - Helpers
     private func getBottomPadding() -> CGFloat {
@@ -135,20 +132,6 @@ struct KarhooBottomSheet<Content: View>: View {
     
     private func getTopPadding() -> CGFloat {
         UIApplication.shared.windows.first?.safeAreaInsets.top ?? 0
-    }
-}
-
-struct KarhooBottomSheetPreviews: PreviewProvider {
-    static var previews: some View {
-        KarhooBottomSheet(
-            viewModel: KarhooBottomSheetViewModel(
-                title: "Some title",
-                onDismissCallback: {
-                })) {
-                    VStack {
-                        Text("Inner View")
-                    }
-                }
     }
 }
 
@@ -166,3 +149,40 @@ final class KarhooBottomSheetScreenBuilder: BottomSheetScreenBuilder {
         return vc
     }
 }
+
+struct KarhooBottomSheetContentWithTextFieldView_Previews: PreviewProvider {
+    static var previews: some View {
+        KarhooBottomSheet(viewModel: KarhooBottomSheetViewModel(
+            title: "Flight number") { }) {
+                KarhooBottomSheetContentWithTextFieldView(viewModel: .init(
+                    contentType: .flightNumber,
+                    initialValueForTextField: "",
+                    viewSubtitle: UITexts.Booking.flightSubtitle,
+                    textFieldHint: UITexts.Booking.flightExample,
+                    errorMessage: UITexts.Booking.onlyLettersAndDigitsAllowedError,
+                    onSaveCallback: { newFlightNumber in return }
+                )
+            )
+        }
+    }
+}
+
+
+struct RoundedCorner: Shape {
+
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
+
+
+extension View {
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape( RoundedCorner(radius: radius, corners: corners) )
+    }
+}
+
+
