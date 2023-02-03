85efd7a7b649162d0cc79413e266b7712ae3835c
Bartlomiej Sopala <bartlomiej.sopala@karhoo.com>
2023-02-03 10:51:18 +0100

    make loyalty subview private

--- KarhooUISDK/Screens/CheckoutScreen/Components/Loyalty/LoyaltyEarnBurnView.swift
+++ KarhooUISDK/Screens/CheckoutScreen/Components/Loyalty/LoyaltyEarnBurnView.swift
@@ -110,6 +110,88 @@ struct LoyaltyEarnBurnView: View {
                 .fill(Color(KarhooUI.colors.primary))
         )
     }
+    
+    private struct LoyaltyErrorView: View {
+        var errorMessage: String
+        var body: some View {
+            VStack(alignment: .leading) {
+                Text(UITexts.Loyalty.title)
+                    .font(Font(KarhooUI.fonts.headerSemibold()))
+                    .foregroundColor(Color(KarhooUI.colors.text))
+                Text(errorMessage)
+                    .font(Font(KarhooUI.fonts.bodyBold()))
+                    .foregroundColor(Color(KarhooUI.colors.textError))
+            }
+            .padding(.all, UIConstants.Spacing.standard)
+            .frame(
+                maxWidth: .infinity,
+                alignment: .leading
+            )
+            .addBorder(Color(KarhooUI.colors.error), cornerRadius: UIConstants.CornerRadius.medium)
+            .background(
+                RoundedRectangle(cornerRadius: UIConstants.CornerRadius.medium)
+                    .fill(Color(KarhooUI.colors.background2))
+            )
+            .alignmentGuide(VerticalAlignment.center) {
+                $0[VerticalAlignment.top]
+            }
+        }
+    }
+    
+    private struct LoyaltyContainerWithBalance<Content: View>: View {
+        
+        let balance: Int
+        @ViewBuilder let content: () -> Content
+        
+        private var balanceTitle: String {
+            String(format: NSLocalizedString(UITexts.Loyalty.balanceTitle, comment: ""), "\(balance)")
+        }
+        
+        var body: some View {
+            ZStack {
+                VStack(alignment: .leading, spacing: UIConstants.Spacing.xSmall) {
+                    Text(UITexts.Loyalty.title)
+                        .font(Font(KarhooUI.fonts.headerSemibold()))
+                        .foregroundColor(Color(KarhooUI.colors.text))
+                    content()
+                }
+                .padding(.all, UIConstants.Spacing.standard)
+                // additional top padding to move title lower than balance view
+                .padding(.top, UIConstants.Spacing.small)
+                .frame(
+                    maxWidth: .infinity,
+                    alignment: .leading
+                )
+                .addBorder(Color(KarhooUI.colors.border), cornerRadius: UIConstants.CornerRadius.medium)
+                .background(
+                    RoundedRectangle(cornerRadius: UIConstants.CornerRadius.medium)
+                        .fill(Color(KarhooUI.colors.background2))
+                )
+                .alignmentGuide(VerticalAlignment.center) {
+                    $0[VerticalAlignment.top]
+                }
+                balanceView
+            }
+        }
+
+        @ViewBuilder
+        private var balanceView: some View {
+            HStack {
+                Spacer()
+                Text(balanceTitle)
+                    .font(Font(KarhooUI.fonts.bodyBold()))
+                    .foregroundColor(Color(KarhooUI.colors.white))
+
+                    .padding(.vertical, UIConstants.Spacing.small)
+                    .padding(.horizontal, UIConstants.Spacing.medium)
+                    .background(
+                        RoundedRectangle(cornerRadius: UIConstants.CornerRadius.medium)
+                            .fill(Color(KarhooUI.colors.primary))
+                    )
+            }
+        }
+    }
+
 }
 
 struct LoyaltyEarnBurnView_Previews: PreviewProvider {
