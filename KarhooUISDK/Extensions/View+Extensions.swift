f8ceb71509abd6d496bc2d7ea76a54127be8162e
Bartlomiej Sopala <bartlomiej.sopala@karhoo.com>
2023-02-14 09:59:18 +0100

    fix textViews and placeholders colors

--- KarhooUISDK/Extensions/View+Extensions.swift
+++ KarhooUISDK/Extensions/View+Extensions.swift
@@ -22,4 +22,23 @@ extension View {
     func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
         clipShape(RoundedCorner(radius: radius, corners: corners))
     }
+
+    func placeholder(
+        _ text: String,
+        when shouldShow: Bool,
+        alignment: Alignment = .leading) -> some View {
+            
+            placeholder(when: shouldShow, alignment: alignment) { Text(text).foregroundColor(KarhooUI.colors.textLabel)}
+    }
+    
+    func placeholder<Content: View>(
+        when shouldShow: Bool,
+        alignment: Alignment = .leading,
+        @ViewBuilder placeholder: () -> Content) -> some View {
+
+        ZStack(alignment: alignment) {
+            placeholder().opacity(shouldShow ? 1 : 0)
+            self
+        }
+    }
 }
