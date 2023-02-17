88c13e7f1bb81b76296ec41f06cc6ef29c97679a
Bartlomiej Sopala <bartlomiej.sopala@karhoo.com>
2023-02-17 15:18:03 +0100

    test commit

--- Client/AppDelegate.swift
+++ Client/AppDelegate.swift
@@ -22,7 +22,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
 
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        // Override point for customization after application launch
         KarhooUI.set(configuration: KarhooConfig())
         #if canImport(Braintree)
             BTAppSwitch.setReturnURLScheme(urlScheme)
