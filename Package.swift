be404a4dcc060cb0830802eceb756d31a54bbd9f
Bartlomiej Sopala <bartlomiej.sopala@karhoo.com>
2023-02-24 14:30:46 +0100

    revert Braintree version bump

--- Package.swift
+++ Package.swift
@@ -26,9 +26,9 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/karhoo/karhoo-ios-sdk", branch: "master"),
         .package(url: "https://github.com/Adyen/adyen-ios", exact: "4.7.1"),
-        .package(url: "https://github.com/braintree/braintree-ios-drop-in", exact: "9.8.0"),
+        .package(url: "https://github.com/braintree/braintree-ios-drop-in", exact: "9.3.0"),
         .package(url: "https://github.com/marmelroy/PhoneNumberKit", exact: "3.3.1"),
-        .package(url: "https://github.com/braintree/braintree_ios", exact: "5.20.1"),
+        .package(url: "https://github.com/braintree/braintree_ios", exact: "5.6.3"),
         .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", exact: "1.9.0"),
         .package(url: "https://github.com/Quick/Quick", exact: "5.0.1"),
         .package(url: "https://github.com/Quick/Nimble", exact: "10.0.0")
