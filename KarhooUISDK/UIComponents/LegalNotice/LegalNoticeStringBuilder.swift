2247603ce96c796886a0c9a6ef1708cbf5d3ae63
dianakarh <86302636+dianakarh@users.noreply.github.com>
2023-02-28 17:36:42 +0200

    MOB-4378 adapt drop in client (#519)
    * configure isExplicitTermsAndConditionsApprovalRequired for all login flows
    
    * add documentation on how the legal notice strings are being used
    
    * add legal notice information on the French version of the Client app

--- KarhooUISDK/UIComponents/LegalNotice/LegalNoticeStringBuilder.swift
+++ KarhooUISDK/UIComponents/LegalNotice/LegalNoticeStringBuilder.swift
@@ -37,7 +37,6 @@ struct LegalNoticeStringBuilder {
             linkAttibutes[.link] = linkUrl
         }
         var text: String {
-            // Replace with `mockLegalNotice` if mockd value needed.
             UITexts.Booking.legalNoticeText
         }
         let fullText = String(format: NSLocalizedString(text, comment: ""), titleForLink)
@@ -50,8 +49,3 @@ struct LegalNoticeStringBuilder {
         return legalNoticeText
     }
 }
-
-private let mockLegalNoticeLink: String = "hexonec587@v3dev.com"
-private let mockLegalNotice: String = """
-Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil hexonec587@v3dev.com, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur.
-"""
