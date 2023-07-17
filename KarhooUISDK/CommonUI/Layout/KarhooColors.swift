400dc0623e0956b5f06922b7ee00d094738abc7a
Diana Petrea <diana.petrea@karhoo.com>
2023-07-17 16:12:39 +0300

    delete deprecated colors

--- KarhooUISDK/CommonUI/Layout/KarhooColors.swift
+++ KarhooUISDK/CommonUI/Layout/KarhooColors.swift
@@ -63,50 +63,7 @@ public protocol KarhooColors {
     var textError: UIColor { get }
 
     // MARK: - Depracated
-//
-//    @available(*, deprecated, message: "Colors are prepared to be used with Dark Mode, therefore, exact colors are no longer supported")
-//    var darkGrey: UIColor { get }
-    
-//    @available(*, deprecated, message: "Colors are prepared to be used with Dark Mode, therefore, dark/light color variants are no longer supported")
-//    var primaryDark: UIColor { get }
-
-//    @available(*, deprecated, message: "Colors are prepared to be used with Dark Mode, therefore, dark/light color variants are no longer supported")
-//    var secondaryDark: UIColor { get }
-
-//    /// InfoColor is used for text or icons associated with informational texts (titles, body, captions, some icons)
-//    @available(*, deprecated, message: "Please use one of colors included in 'Text Colors' section")
-//    var infoColor: UIColor { get }
-//
-//    /// PrimaryTextColor is used for informational texts
-//    @available(*, deprecated, renamed: "text")
-//    var primaryTextColor: UIColor { get }
-//
-//    @available(*, deprecated, message: "")
-//    var medGrey: UIColor { get }
-
-//    /// The light grey is used for text backgrounds
-//    @available(*, deprecated, message: "")
-//    var lightGrey: UIColor { get }
-    
-//    /// The regular grey is used for borders for cards, line dividers and unselected and incomplete inputs
-//    @available(*, deprecated, message: "")
-//    var regularGrey: UIColor { get }
-
-//    @available(*, deprecated, message: "")
-//    var offWhite: UIColor { get }
 
     @available(*, deprecated, message: "")
     var white95: UIColor { get }
-
-//    @available(*, deprecated, message: "")
-//    var neonRed: UIColor { get }
-//
-//    @available(*, deprecated, message: "")
-//    var mapTripPath: UIColor { get }
-//
-//    @available(*, deprecated, message: "")
-//    var mapTripPathBorder: UIColor { get }
-    
-//    @available(*, deprecated, message: "")
-//    var guestCheckoutLightGrey: UIColor { get }
 }
