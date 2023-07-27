7f229ba597ef777193b5fc558f352ae37e8a92f0
dianakarh <86302636+dianakarh@users.noreply.github.com>
2023-07-27 17:24:33 +0300

    fix country json inclusion in bundle for SPM (#604)

--- Package.swift
+++ Package.swift
@@ -38,7 +38,8 @@ let package = Package(
             name: "KarhooUISDK",
             dependencies: [.product(name: "KarhooSDK", package: "karhoo-ios-sdk")],
             path: "KarhooUISDK",
-            exclude: ["Extensions/Bundle+extensions/Bundle+current.swift", "Info.plist"]),
+            exclude: ["Extensions/Bundle+extensions/Bundle+current.swift", "Info.plist"],
+            resources: [.process("Workers/CountryAndPhone/country_phone_validation_rules.json")]),
 
         .target(
             name: "KarhooUISDKAdyen",
